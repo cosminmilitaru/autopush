6278d462e18f2662121e1488064845cb45495b68
Cosmin Militaru <cmilitaru@evolution.com>
2024-01-30 12:02:08 +0200

    test

--- autopush.bat
+++ autopush.bat
@@ -3,7 +3,7 @@ echo Batch Script to take input.
 set /p input= Type git message :  
 git pull
 git add .
-git commit -m "Changes committed: $TODAY from $HOST"
+git commit -m %input%
 git push
 git pull
 echo Input is: %input%
