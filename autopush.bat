e849ae2afc069baa23c76166595f6781bb0762c3
Cosmin Militaru <cmilitaru@evolution.com>
2024-01-31 11:47:46 +0200

    remove test.txt

--- autopush.bat
+++ autopush.bat
@@ -3,7 +3,7 @@ echo Starting git autopush...
 set /p input= Git commit message :  
 git pull
 @REM git rm --cached autopush.bat
-git rm --cached test.txt
+@REM git rm --cached test.txt
 git add .
 git commit -m "%input%"
 git push
